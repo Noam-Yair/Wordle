3a4db589d101da96293df74a2117e8c93e33eb4f
Noam Yair <noam0906@gmail.com>
2022-11-15 16:02:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import Grid from './components/Grid';
+import Header from './components/Header';
+import styles from './App.css';
+import Keyboard from './components/Keyboard';
+import {useState, useEffect} from 'react'
+import GameManager from './components/GameManager';
 
 function App() {
+  // const [letter, setLetter] = useState("")
+  // const [selectedLetters, setSelectedLetters] = useState([]);
+  // let [curRow, setRow] = useState(0);
+  // const [curColumn, setColumn] = useState(-1);
+  // const [previousWords, setPreviousWords] = useState([])
+  
+  // const secretWord = "state"
+
+  // useEffect(() => {
+  //   loopThrough();
+  // }, [letter])
+  
+  // const loopThrough = () => {
+  //   letter != "" && selectedLetters.push(letter)
+  //   for (let i=0; i<selectedLetters.length; i++){
+  //     var cellToChange = document.getElementsByClassName(`${curRow},${i}`);
+  //     if (cellToChange) {
+  //       var newLetter = selectedLetters[i]
+  //       console.log("here")
+  //       console.log(newLetter)
+  //       // var newHTML = "<th key=" + `${curRow},${i}` + ">" + selectedLetters[i] + "</th>";
+  //       cellToChange.innerHTML = newLetter
+  //   }
+      
+  //   }
+  // }
+
+  // const checkForWord = () => {
+  //   let guessWord = selectedLetters.join('');
+  //   if (guessWord == secretWord){
+  //       console.log("You Won!")
+  //   }
+  //   else {
+  //     setRow(++curRow)
+  //     setColumn(0);
+  //     setSelectedLetters([])
+  //     setLetter("")
+  //     setPreviousWords(...previousWords, selectedLetters)
+  //  }
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GameManager/>
+    {/* <Header/>
+    <Grid curCol={curColumn} curRow={curRow} letter={letter} selectedLetters={selectedLetters}/>
+    <Keyboard select={setLetter} selected={letter} curCol={curColumn} setColumn={setColumn} setRow={setRow} curRow={curRow} handleSubmit={checkForWord}/> */}
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            <div className="container">
+                <h2>
+                    Wordle
+                </h2>
+            </div>
+            
+        </header>
+    )
+}
+    
+export default Header
